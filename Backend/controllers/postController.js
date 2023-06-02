423dfddf9e976d577166c3b3c7088bed167a77cc
hazemboughraira <hboughraira@rdmc.dev>
2023-06-02 15:47:40 +0100

    fixes in ws comments

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -3,30 +3,32 @@ const Post = require("../models/postModel");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
 exports.createPost = asyncErrorHandler(async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const image = req.file.path;
-        const userId = req.body.user; 
-        const post = new Post({
-          title,
-          description,
-          image,
-          user: userId 
-        });
-    
-        await (await post.save()).populate('user');
-    
-        res.status(200).json(post);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
-})
+  try {
+    const { title, description } = req.body;
+    const image = req.file.path;
+    const userId = req.body.user;
+    const post = new Post({
+      title,
+      description,
+      image,
+      user: userId,
+    });
+
+    await (await post.save()).populate("user");
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 exports.getPost = asyncErrorHandler(async (req, res) => {
   const { postId } = req.params;
 
   try {
-    const post = await Post.findById(postId).populate('user');
+    const post = await Post.findById(postId)
+      .populate("user")
+      .populate({ path: "comments", populate: { path: "user" } });
 
     if (!post) {
       return res.status(404).json({ message: "post not found" });
@@ -40,7 +42,7 @@ exports.getPost = asyncErrorHandler(async (req, res) => {
 
 exports.updatePost = asyncErrorHandler(async (req, res, next) => {
   const { postId } = req.params;
-  const { title, description,userId } = req.body;
+  const { title, description, userId } = req.body;
   const image = req.file.path;
   try {
     const post = Post.findById(postId);
@@ -48,7 +50,7 @@ exports.updatePost = asyncErrorHandler(async (req, res, next) => {
     post.title = title;
     post.description = description;
     post.image = image;
-    post.user=userId
+    post.user = userId;
     await post.save();
 
     res.status(200).json({ message: "Post updated successfully", post });
@@ -77,7 +79,9 @@ exports.deletePost = asyncErrorHandler(async (req, res) => {
 
 exports.getPosts = asyncErrorHandler(async (req, res) => {
   try {
-    const posts = await Post.find().populate('user');
+    const posts = await Post.find()
+      .populate("user")
+      .populate({ path: "comments", populate: { path: "user" } });
 
     res.json(posts);
   } catch (err) {
@@ -87,97 +91,93 @@ exports.getPosts = asyncErrorHandler(async (req, res) => {
 });
 
 exports.createComment = asyncErrorHandler(async (req, res) => {
-    try {
-        const { text, user } = req.body;
-        const postId = req.params.postId;
-    
-        const comment = new Comment({
-          text,
-          user: user
-        });
-    
-        await comment.save();
-    
-        const post = await Post.findById(postId);
-        post.comments.push(comment);
-        await post.save();
-    
-        res.status(200).json(comment);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
+  try {
+    const { text, user } = req.body;
+    const postId = req.params.postId;
 
-})
+    const comment = new Comment({
+      text,
+      user: user,
+    });
+
+    await comment.save();
+
+    const post = await Post.findById(postId);
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 exports.getAllPostComments = asyncErrorHandler(async (req, res) => {
+  try {
+    const postId = req.params.postId;
 
-    try {
-        const postId = req.params.postId;
-    
-        const post = await Post.findById(postId).populate('comments');
-        if (!post) {
-          return res.status(404).json({ error: 'Post not found' });
-        }
-    
-        res.json(post.comments);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
-})
+    const post = await Post.findById(postId).populate({ path: "comments", populate: { path: "user" } });;
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.json(post.comments);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 exports.deleteComment = asyncErrorHandler(async (req, res) => {
-    try {
-        const postId = req.params.postId;
-        const commentId = req.params.commentId;
-    
-        const post = await Post.findById(postId);
-        if (!post) {
-          return res.status(404).json({ error: 'Post not found' });
-        }
-    
-        const commentIndex = post.comments.indexOf(commentId);
-        if (commentIndex === -1) {
-          return res.status(404).json({ error: 'Comment not found' });
-        }
-    
-        post.comments.splice(commentIndex, 1);
-        await post.save();
-    
-        await Comment.findByIdAndDelete(commentId);
-    
-        res.sendStatus(200);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
+  try {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
 
-})
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const commentIndex = post.comments.indexOf(commentId);
+    if (commentIndex === -1) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    post.comments.splice(commentIndex, 1);
+    await post.save();
+
+    await Comment.findByIdAndDelete(commentId);
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 exports.updateComment = asyncErrorHandler(async (req, res) => {
-    try {
-        const postId = req.params.postId;
-        const commentId = req.params.commentId;
-        const { text, user } = req.body;
-    
-        const post = await Post.findById(postId);
-        if (!post) {
-          return res.status(404).json({ error: 'Post not found' });
-        }
-    
-        const comment = await Comment.findById(commentId);
-        if (!comment) {
-          return res.status(404).json({ error: 'Comment not found' });
-        }
-    
-        
-        if (comment.user.toString() !== user) {
-          return res.status(401).json({ error: 'Unauthorized' });
-        }
-    
-        comment.text = text;
-        await comment.save();
-    
-        res.json(comment);
-      } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-      }
-})
+  try {
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    const { text, user } = req.body;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    if (comment.user.toString() !== user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    comment.text = text;
+    await comment.save();
+
+    res.json(comment);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
